a4a1c56c2737ed74f0f1fbdb595d8efbb377bb48
Chris Harris <cHarris505@gmail.com>
2023-08-10 19:50:40 -0600

    wrote the psuedo code for index.js.

---
+++ index.js
@@ -0,0 +1,11 @@
+// Define the required npm extensions
+
+// Write the function for terminal prompts
+
+// Write the function for applying the answers to the appropriate SVG template
+
+// Write the code for applying the appropriate SVG markup
+
+// Write the code to create the SVG file
+
+// Write the code to run the application
